7181cd7bb0060beedecfb6bd4124b2516939861a
ace68723 <ace68723@gmail.com>
2015-05-24 17:01:02 -0400

    git ignore node/connection

--- node/connection/dbMysql.js
+++ node/connection/dbMysql.js
@@ -18,7 +18,7 @@ function Mysql() {
           connectionLimit : 30,
           host            : 'localhost',
           user            : 'root',
-          password        : '',
+          password        : 'MySmartSQL',
           database        : 'node'    
     });      
 };
