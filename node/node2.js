9758c2125e794f0856dac1039422d6d5e3691eaa
ace68723 <ace68723@gmail.com>
2015-05-24 23:59:59 -0400

    node2 job test

--- node/node2.js
+++ node/node2.js
@@ -74,7 +74,7 @@ var getTables = function() {
    			
    			console.log('finally')
    	})
-   	
+
    	return deferred.promise;	
 };
 
@@ -89,38 +89,31 @@ var getTables = function() {
 
 
 
-// var drivers = [
-// { 	"did": "Xunrui", 
-// 	"available": 1432516683235, 
-// 	"off": 1432519683235, 
-// 	"location": "43.624203,-79.485100"
-// },
+var drivers = [
+{ "did": "Xunrui", "available": 100, "off": 600, "location": "Chanmao Inc." },
+{ "did": "Aiden", "available": 5, "off": 400, "location": "Chanmao Inc." }
+];
 
-// { 	"did": "Aiden", 
-// 	"available": 1432516683235, 
-// 	"off": 1432519683235,  
-// 	"location": "43.624203,-79.485100"}
-// ];
+var tasks = [ 
+{ "tid":"order1-fetch", "location":"Chanmao Inc.", "deadline":110, "did":"", "depend":"" }, 
+{ "tid":"order1-deliver", "location":"Client1", "deadline":210, "did":"", "depend":"order1-fetch" }, 
+{ "tid":"order2-deliver", "location":"Client2", "deadline":310, "did":"Aiden", "depend":"" } 
+];
 
-// var tasks = [ 
-// { "tid":"order1-fetch", "location":"Chanmao Inc.", "deadline":110, "did":"", "depend":"" }, 
-// { "tid":"order1-deliver", "location":"Client1", "deadline":210, "did":"", "depend":"order1-fetch" }, 
-// { "tid":"order2-deliver", "location":"Client2", "deadline":310, "did":"Aiden", "depend":"" } 
-// ];
+var paths = [ 
+{ "start":"Chanmao Inc.", "end":"Client1", "time":10},
+{ "end":"Chanmao Inc.", "start":"Client1", "time":10},
+{ "start":"Chanmao Inc.", "end":"Client2", "time":20},
+{ "end":"Chanmao Inc.", "start":"Client2", "time":20},
+{ "start":"Client1", "end":"Client2", "time":25},
+{ "end":"Client1", "start":"Client2", "time":25}
+];
 
-// var paths = [ 
-// { "start":"Chanmao Inc.", "end":"Client1", "time":10},
-// { "end":"Chanmao Inc.", "start":"Client1", "time":10},
-// { "start":"Chanmao Inc.", "end":"Client2", "time":20},
-// { "end":"Chanmao Inc.", "start":"Client2", "time":20},
-// { "start":"Client1", "end":"Client2", "time":25},
-// { "end":"Client1", "start":"Client2", "time":25}
-// ];
 
-// function callbackFunc(str) {
-// 	console.log(str);
-// }
+function callbackFunc(str) {
+	console.log(str);
+}
 
-// jobSchedule.search(JSON.stringify({"curTime":10, "drivers":drivers, "tasks":tasks, "paths":paths}), callbackFunc);
-// //console.log(jobSchedule.search(JSON.stringify({"drivers":drivers, "tasks":tasks, "paths":paths})));
+jobSchedule.search(JSON.stringify({"curTime":10, "drivers":drivers, "tasks":tasks, "paths":paths}), callbackFunc);
+//console.log(jobSchedule.search(JSON.stringify({"drivers":drivers, "tasks":tasks, "paths":paths})));
 
